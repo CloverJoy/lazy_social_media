fa4ff279d5340524b2019f8bb454ce1618821517
CloverJoy <chris.ang.dev@gmail.com>
2021-03-16 00:23:59 -0700

    Now caption with # and added image resizing

--- akp_scraper/app.py
+++ akp_scraper/app.py
@@ -1,11 +1,18 @@
 import requests, datetime, threading, os, urllib.request
 from bs4 import BeautifulSoup
 from instabot import Bot 
+from PIL import Image
 
 bot = Bot()
-# bot.login()
+bot.login()
 current_headline = ""
 current_image = "" 
+
+def image_resizer(img):
+    imgstr = img
+    img = Image.open(img)
+    img.thumbnail((400,400))
+    img.save("resized_"+ imgstr)
 
 def scrap_akp():
     response = requests.get("https://www.allkpop.com/")
@@ -15,7 +22,12 @@ def scrap_akp():
     images = soup.select(".image")
     categories = soup.select(".category")
     newest_headline = headlines[1].select(".h_a_i")[0].getText()
-    imageurl = images[1].select(".b-lazy")[0].get("data-src")
+    hashtag_list = newest_headline.split(' ')
+    hashtag_list[0] = f"#{hashtag_list[0]}"
+    hashtag = " #".join(hashtag_list) + "#kpop #kpopnews #kdrama #kwave"
+    print(hashtag)
+
+    imageurl = images[1].select(".b-lazy")[0].get("data-src").replace('thumb/','')
     category = categories[1].getText()
     if (imageurl[-4:] == "jpeg"):
         imagename = "image." + imageurl[-4:]
@@ -38,11 +50,14 @@ def scrap_akp():
     else:
         if category == "News":
             current_headline = newest_headline
-            current_image = imagename
             print(f"updated headline: {current_headline}, category: {category}")
             urllib.request.urlretrieve(imageurl, imagename)
-            bot.upload_photo(imagename, 
-                    caption = f"{current_headline}. Source: allkpop")
+            print(imagename)
+            image_resizer(imagename)
+            current_image = "resized_"+imagename
+            os.remove(imagename)
+            bot.upload_photo(current_image, 
+                    caption = f"{current_headline}.\nSource: allkpop. \n.\n.\n.\n{hashtag}")
         else:
              current_headline = newest_headline
              print(f"No update: {newest_headline}, category: {category}")
